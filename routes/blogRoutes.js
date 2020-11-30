f97b1431b2e89b4986ee2621147a0f43f3c26030
richCode <chineduogada@gmail.com>
2020-11-30 22:05:57 +0100

    Hydrating Models

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,38 +1,63 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
 
-const Blog = mongoose.model('Blog');
+const Blog = mongoose.model("Blog");
 
-module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
-    const blog = await Blog.findOne({
-      _user: req.user.id,
-      _id: req.params.id
-    });
+module.exports = (app) => {
+	app.get("/api/blogs/:id", requireLogin, async (req, res) => {
+		const blog = await Blog.findOne({
+			_user: req.user.id,
+			_id: req.params.id,
+		});
 
-    res.send(blog);
-  });
+		res.send(blog);
+	});
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+	// app.get("/api/blogs", requireLogin, async (req, res) => {
+	// 	const redis = require("redis");
+	// 	const redisUrl = "redis://127.0.0.1:6379";
+	// 	const client = redis.createClient(redisUrl);
+	// 	const { promisify } = require("util");
+	// 	client.get = promisify(client.get);
 
-    res.send(blogs);
-  });
+	// 	// Do we have any cached data related to this query
+	// 	const cachedBlogs = await client.get(req.user.id);
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
-    const { title, content } = req.body;
+	// 	// If yes, then respond to the request right away and return;
 
-    const blog = new Blog({
-      title,
-      content,
-      _user: req.user.id
-    });
+	// 	if (cachedBlogs) {
+	// 		console.log("SERVING FROM CACHE");
+	// 		return res.send(JSON.parse(cachedBlogs));
+	// 	}
 
-    try {
-      await blog.save();
-      res.send(blog);
-    } catch (err) {
-      res.send(400, err);
-    }
-  });
+	// 	// If no, we need to respond request and update our cache to store the data
+	// 	const blogs = await Blog.find({ _user: req.user.id });
+	// 	console.log("SERVING FROM MONGODB");
+	// 	res.send(blogs);
+
+	// 	client.set(req.user.id, JSON.stringify(blogs));
+	// });
+
+	app.get("/api/blogs", requireLogin, async (req, res) => {
+		const blogs = await Blog.find({ _user: req.user.id });
+		res.send(blogs);
+	});
+
+	app.post("/api/blogs", requireLogin, async (req, res) => {
+		const { title, content } = req.body;
+
+		const blog = new Blog({
+			title,
+			content,
+			_user: req.user.id,
+		});
+
+		try {
+			await blog.save();
+			res.send(blog);
+		} catch (err) {
+			res.send(400, err);
+		}
+	});
 };
+
---
+++ services/cache.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const redis = require("redis");
+const { promisify } = require("util");
+
+// Setup REDIS
+const redisUrl = "redis://localhost:6379";
+const client = redis.createClient(redisUrl);
+client.get = promisify(client.get);
+
+// Reference the Original mongoose prototypal `exec` method
+const exec = mongoose.Query.prototype.exec;
+
+// Monkey-patch (hijack) the mongoose prototypal `exec` method
+/**
+ * @returns { Promise }
+ */
+mongoose.Query.prototype.exec = async function () {
+	// client.flushall();
+	// Create a unique but consistent `cache-key`
+	const cacheKey = JSON.stringify(
+		Object.assign({}, this.getQuery(), {
+			collection: this.mongooseCollection.name,
+		})
+	);
+
+	// Get the already `cached-value` based off the `cache-key` from the `redis-server`
+	let cacheValue = await client.get(cacheKey);
+
+	if (cacheValue) {
+		cacheValue = JSON.parse(cacheValue);
+		let doc;
+
+		// We have to query mongoose in other to get a Document;
+		// eg new Blog(<filter: plain JSON>) --> new this.model(cacheValue: plain JSON)
+
+		// If the value which was cached is an Array object. this means we must return an array on mongoose Docs
+		if (Array.isArray(cacheValue)) {
+			doc = cacheValue.map((d) => new this.model(d));
+		} else {
+			doc = new this.model(cacheValue);
+		}
+
+		// Have to return a mongoose Document and not a plain JSON(from cache)
+		return doc;
+	}
+
+	// If there isn't no `cached-value` that means we'll to reach-out to MONGO
+	const execDoc = await exec.apply(this, arguments);
+
+	// and then set`cached-value` to `redis-server`
+	client.set(cacheKey, JSON.stringify(execDoc));
+
+	return execDoc;
+};
+
