7ecff38335901e6443ba0a3ecd1bfebc926bbb45
richCode <chineduogada@gmail.com>
2020-12-01 19:09:58 +0100

    extracting page content

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,15 +1,21 @@
 const puppeteer = require("puppeteer");
 
-test("Adds two number", () => {
-	const sum = 1 + 2;
+// test("Adds two number", () => {
+// 	const sum = 1 + 2;
 
-	expect(sum).toEqual(3);
-});
+// 	expect(sum).toEqual(3);
+// });
+
+beforeAll(() => jest.setTimeout(500000));
 
 test("We can launch a Browser", async () => {
 	const browser = await puppeteer.launch({ headless: false });
 	const page = await browser.newPage();
 
-	await page.goto("localhost:3000");
+	await page.goto("http://127.0.0.1:3000");
+
+	const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
+
+	expect(text).toBe("Blogster");
 });
 
