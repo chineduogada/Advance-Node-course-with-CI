e3465376b87ef4de5ba724b2a24df21040bb3a33
richCode <chineduogada@gmail.com>
2020-12-01 05:04:20 +0100

    launching chromium intances

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,6 +1,13 @@
+const puppeteer = require("puppeteer");
+
 test("Adds two number", () => {
 	const sum = 1 + 2;
 
 	expect(sum).toEqual(3);
+});
+
+test("We can launch a Browser", async () => {
+	const browser = await puppeteer.launch({ headless: false });
+	const page = await browser.newPage();
 });
 
