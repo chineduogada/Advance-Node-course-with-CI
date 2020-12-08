4222445feb43793a9f8e8b0a20b1962d54839b7e
richCode <chineduogada@gmail.com>
2020-12-08 16:55:50 +0100

    generating sessions and signatures

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,7 @@
 const puppeteer = require("puppeteer");
+const Buffer = require("safe-buffer").Buffer;
+const Keygrip = require("keygrip");
+const keys = require("../config/keys");
 
 beforeAll(() => jest.setTimeout(500000));
 
@@ -14,20 +17,41 @@ beforeEach(async () => {
 // afterEach(async () => await browser.close());
 
 describe("Header", () => {
-	test("has a correct `brand` text", async () => {
-		const brand = await page.$eval("a.brand-logo", (el) => el.innerHTML);
+	// test("has a correct `brand` text", async () => {
+	// 	const brand = await page.$eval("a.brand-logo", (el) => el.innerHTML);
 
-		expect(brand).toBe("Blogster");
-	});
+	// 	expect(brand).toBe("Blogster");
+	// });
 
-	test("clicking login starts OAuth flow", async () => {
-		await page.click('.right [href="/auth/google"]');
+	// test("clicking login starts OAuth flow", async () => {
+	// 	await page.click('.right [href="/auth/google"]');
 
-		const url = await page.url();
+	// 	const url = await page.url();
 
-		console.log(url);
+	// 	// expect(url).toContain("accounts.google.com");
+	// 	expect(url).toMatch(/accounts\.google\.com/i);
+	// });
 
-		// expect(text).toBe("Blogster");
+	test("when signed in, shows `logout` button", async () => {
+		const keygrip = new Keygrip([keys.cookieKey]);
+
+		const user = "5fbbd86a764e500728c768ce";
+		const sessionObj = {
+			passport: { user },
+		};
+
+		// use Buffer to get the base64 encode (the `session` itself);
+		const session = Buffer.from(JSON.stringify(sessionObj)).toString(
+			"base64"
+		);
+
+		// use Keygrip to `sign` the `session`
+		const signStr = "session=" + session;
+		const sessionSig = keygrip.sign(signStr);
+
+		console.log(session, sessionSig);
+
+		expect("2").toBe("2");
 	});
 });
 
